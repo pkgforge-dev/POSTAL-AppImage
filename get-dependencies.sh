f61f78c236f61fa9cf8091cd54fa4ea7d5a89406
Link4Electronics <link4electronics@gmail.com>
2026-04-13 03:27:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,29 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building POSTAL..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/RWS-Studios/POSTAL-SourceCode"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./POSTAL
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./POSTAL
+patch -Np1 -i ../fixes.patch
+if [ "$ARCH" = "aarch64" ]; then
+    sed -i 's|CLIENTEXE := $(BINDIR)/postal1-x86_64|CLIENTEXE := $(BINDIR)/postal1-aarch64|' makefile
+fi
+make -j$(nproc)
+mv -v bin/postal1-$ARCH ../AppDir/bin/postal1
+sed -i -e '/DeviceBufTime = 200/a DeviceRate = 22050\nDeviceBits = 16' \
+       -e 's|File = res\\levels\\realms.ini|File = res/levels/postal_plus_realms.ini|' \
+       -e 's/RecentDifficulty = 11/RecentDifficulty = 5/' \
+       -e 's/UseMouse[[:space:]]*= 0/UseMouse = 1/' DefaultPostal.ini
+mv -v DefaultPostal.ini ../AppDir/bin/POSTAL.INI
