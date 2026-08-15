01dbabb895d5b738e1c8c9bb54ff6745a167151c
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:51:55 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,8 +12,6 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/postal1
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
