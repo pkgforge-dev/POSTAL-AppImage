dccbe1ab3e24996c03151c9b9e6b1ef0db1ad9b2
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:37:33 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-echo "Making nightly build of POSTAL..."
+echo "Building POSTAL..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/RWS-Studios/POSTAL-SourceCode"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
